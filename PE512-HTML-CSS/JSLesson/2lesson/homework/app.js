33ebdc178541599191d2b9b5827cc685a337cbf5
Valeriyshin <valeriy.shin.s@gmail.com>
2026-04-26 19:36:44 +0500

    Циклы

---
+++ PE512-HTML-CSS/JSLesson/2lesson/homework/app.js
@@ -0,0 +1,152 @@
+// let i = 1
+// while (i<=5){
+//     console.log(i)
+//     i++
+// }
+
+// let i = 5
+// while (i>=1){
+//     console.log(i)
+//     i--
+// }
+
+// let i = 2
+// while(i <= 20){
+//     console.log(i)
+//     i+=2
+// }
+
+// let i = 1
+// let sum = 0
+// while(i<=5){
+//     sum+=i
+//     i++
+// }
+// console.log(sum)
+
+// let input=""
+// while(input !== "exit"){
+//     input=prompt("Введите exit для выхода")
+// }
+
+// let numder=Number(prompt("Введите больше 10"))
+// while(numder<=10){
+//     numder=Number(prompt("Ошибка введите снова"))
+// }
+
+// for (let i = 1; i<= 5;i++){
+//     console.log(i)
+// }
+
+
+// for (let i = 5; i>= 1;i--){
+//     console.log(i)
+// }
+
+// for(let i = 1; i<=20; i+=5){
+//     console.log(i)
+// }
+
+// let sum=0
+// for(let i =1;i<10;i++){
+//     sum+=i
+// }
+// console.log(sum)
+
+// let i = 1
+// do{
+//     console.log(i)
+//     i++
+// }
+// while (i<=5)
+
+// let i = 1
+// do{
+//     console.log(i)
+//     i++
+// }
+// while (false)
+
+// let choice
+// do{
+//     choice= prompt("1-Hello;2-exit")
+//     if (choice==="1"){
+//         console.log("Hello")
+//     }
+// }
+// while(choice!=="2")
+
+// for (let i = 1;i<=3;i++){
+//     for(let j =1; j<=3;j++){
+//         console.log(i,j)
+//     }
+// }
+
+// for (let i =1; i<=3;i++){
+//     let row = ""
+
+//     for(let j=1;j<=3;j++){
+//         row+="*"
+//     }
+//     console.log(row)
+// }
+
+// for (let i =1;i<=5;i++){
+//     let row=""
+//     for(let j=1;j<=i;j++){
+//         row+="*"
+//     }
+//       console.log(row)  
+// }
+
+// for(let i=0;i<8;i++){
+//     let row=""
+//     for (let j=0;j<8;j++){
+//         if ((i+j)%2===0){
+//             row+="⬛"
+//         }
+//         else{
+//             row+="⬜"
+//         }
+//     }
+//     console.log(row)
+// }
+
+
+// let i = 1
+// while (i<=10){
+   
+//     if(i%2!=0){
+//         console.log(i)
+        
+//     } 
+//      i++ 
+// }
+
+// for (let i = 1;i<=10;i++){
+//     if (i%3==0){
+//         console.log(i)
+//     }
+// }
+
+// let choice
+// do{
+//     choice= prompt("0-Hello;1-exit;2-game")
+//     if (choice==="0"){
+//         console.log("Hello")
+//     }
+//     else if (choice==="0"){
+//         console.log("game")
+// }
+// }
+
+// while(choice!=="1")
+
+
+// for (let i =1;i<=5;i++){
+//     let row=""
+//     for(let j=1;j<=i;j--){
+//         row+="*"
+//     }
+//       console.log(row)  
+// }
